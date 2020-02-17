577149b6b932541732520cf64e5a4c8600ab4b48
Eugene Karmazin <eugene@tetravr.com>
2020-02-17 14:52:27 -0500

    fix fqdn list validation

--- main.tf
+++ main.tf
@@ -15,12 +15,12 @@ resource "aws_route53_record" "this" {
   name    = aws_acm_certificate.this.domain_validation_options[0].resource_record_name
   type    = aws_acm_certificate.this.domain_validation_options[0].resource_record_type
   zone_id = data.aws_route53_zone.zone.id
-  records = aws_acm_certificate.this.domain_validation_options[0].resource_record_value
+  records = [aws_acm_certificate.this.domain_validation_options[0].resource_record_value]
   ttl     = 60
 }
 
 resource "aws_acm_certificate_validation" "dns_validation" {
   certificate_arn         = aws_acm_certificate.this.arn
-  validation_record_fqdns = aws_route53_record.this.fqdn
+  validation_record_fqdns = flatten(["${aws_route53_record.this.fqdn}"])
 }
 
